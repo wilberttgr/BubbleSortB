61af48a76a41130b171ce4916044d1d179479070
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-10-06 20:27:03 +0700

    method1

--- BubbleSortB/Program.cs
+++ BubbleSortB/Program.cs
@@ -8,6 +8,39 @@ namespace BubbleSortB
 {
     class Program
     {
-        
+        // Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
+
+        // Deklarasi variabel int untuk menyimpan banyaknya data pada array
+        private int n;
+
+        // Fungsi / Method untuk menerima masukan
+        public void read()
+        {
+            // Menerima angka untuk menentukan banyaknya data yang disimpan pada array
+            while (true)
+            {
+                Console.Write("Masukkan banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
+            }
+                Console.WriteLine("");
+                Console.WriteLine("-----------------------");
+                Console.WriteLine(" Masukkan elemen array ");
+                Console.WriteLine("-----------------------");
+
+                // Pengguna memasukkan elemen pada array
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("<" + (i + 1) + "> ");
+                    string s1 = Console.ReadLine();
+                    a[i] = Int32.Parse(s1);
+                }
+            }
+        }
     }
-}
+
