fe87df15dfc5d5985b90d9d57262c273f2a9d7f0
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-10-06 20:35:06 +0700

    method2

--- BubbleSortB/Program.cs
+++ BubbleSortB/Program.cs
@@ -41,6 +41,24 @@ namespace BubbleSortB
                     a[i] = Int32.Parse(s1);
                 }
             }
+        public void display()
+        {
+            // Menampilkan array yang tersusun 
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine(" Element array yang telah tersusun ");
+            Console.WriteLine("-----------------------------------");
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(a[j]);
+            }
+            Console.WriteLine("");
         }
-    }
+
+                
+
+                }
+            }
+        
+    
 
